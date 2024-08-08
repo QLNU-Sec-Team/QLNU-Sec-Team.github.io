7ef6bfbe9c1a1ac548ccaa3667496a63001031c7
Yalois <2051703348@qq.com>
2024-08-08 23:54:54 +0800

    修改布局，修改部分css，优化部分信息

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	server:{
+		host:'0.0.0.0',
+		port:4321
+	}
+});
